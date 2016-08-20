63246efb985fbe181094a5fe035c2227c04290e0
Eric Geniesse <egeniesse@proteus.com>
2016-08-20 08:20:26 -0700

    Updating the built in plugins

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -7,6 +7,9 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+set tabstop=2 softtabstop=0 expandtab shiftwidth=2 smarttab
+set number
+
 try
 source ~/.vim_runtime/my_configs.vim
 catch
